fe05c256825cfe54a2e2a569e9bfc5dce34f463a
Abhishek <username>
2024-11-10 23:19:49 +0545

    function and datatype

---
+++ Operations.js
@@ -0,0 +1,24 @@
+let value =3;
+let negValue = -value
+console.log(negValue);
+
+// console.log(3+4);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+ let str1= "abhi"
+ let str2= "sh"
+    console.log(str1+str2)
+    console.log("1"+2);
+    console.log(1+"2");
+    console.log("1"+2+2);
+    console.log(1+2+"2");
+    console.log(1+2+"2"+2+2);
+    console.log(true);
+    console.log(+true);
+    console.log(+"");
+   // console.log(true+);// error
+   
