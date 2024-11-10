769ffb334248520ae07033286b1f225f18e52e87
Abhishek <username>
2024-11-10 20:24:59 +0545

    Datatype_Conversion

---
+++ Datatype_Conversion.js
@@ -0,0 +1,28 @@
+let score = 'abhi';
+
+console.log(typeof score)
+console.log(typeof (score)) // "33"
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 0 => false
+// 1 => true
+// "" => false
+// "abhi" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
+
